1ea94b1cc055c5df341bb5170f7f3826d247c2fa
launchvelocity-eng <mongealfredoantonio@gmail.com>
2026-08-13 22:21:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-let pool = null;
-if (process.env.DATABASE_URL) {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-  });
-  console.log('Database pool initialized.');
-} else {
-  console.log('Warning: DATABASE_URL not found. Running without active DB connection.');
-}
+import pkg from 'pg';
+const { Pool } = pkg;
